80142fdc6e45d321038bdae5b06a08af83ff8e04
Qobi1 <tolaganov03@list.ru>
2022-05-23 15:23:49 +0500

    nimadir

--- shop/shop_app/models.py
+++ shop/shop_app/models.py
@@ -2,3 +2,47 @@ from django.db import models
 
 # Create your models here.
 
+
+def availibility():
+    return [
+        ('In Stock', 'In Stock'),
+        ('In Cash', 'In Cash'),
+        ('By Credit Card', 'By Credit Card'),
+    ]
+
+
+def rate():
+    return [
+        ('⭐', '⭐'),
+        ('⭐⭐', '⭐⭐'),
+        ('⭐⭐⭐', '⭐⭐⭐'),
+        ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
+        ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐')
+    ]
+
+
+def price():
+    return [
+        ('$', '$'),
+        ('€', '€'),
+        ('UZS', 'UZS'),
+    ]
+
+
+class Category(models.Model):
+    image = models.ImageField()
+    content = models.CharField(max_length=100)
+    price_symbol = models.CharField(max_length=56, choices=price())
+    price = models.IntegerField()
+
+
+class Product(models.Model):
+    ctg = models.ForeignKey(Category, on_delete=models.CASCADE)
+    availibility = models.CharField(max_length=56, choices=availibility())
+    rate = models.CharField(max_length=56, choices=rate())
+    short_info = models.TextField()
+    color = models.CharField(max_length=56)
+    size = models.CharField
+    description = models.TextField()
+
+
--- shop/shop_app/urls.py
+++ shop/shop_app/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('error/', error, name='index'),
-    path('about/', about, name='index'),
-    path('blog-post/', blog_post, name='index'),
-    path('blog/', blog, name='index'),
-    path('cart/', cart, name='index'),
-    path('checkout/', checkout, name='index'),
-    path('contact/', contact, name='index'),
-    path('index2/', index2, name='index'),
-    path('shop/', shop, name='index'),
-    path('shop-single', shop_single, name='index'),
+    path('', index, name='home'),
+    path('error/', error, name='error'),
+    path('about/', about, name='about'),
+    path('blog-post/', blog_post, name='blog_post'),
+    path('blog/', blog, name='blog'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
+    path('contact/', contact, name='contact'),
+    path('index2/', index2, name='index2'),
+    path('shop/', shop, name='shop'),
+    path('shop_single/', shop_single, name='shop_single'),
 ]
