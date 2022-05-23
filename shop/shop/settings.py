f8be9f601724f04f49d99ec8b7b5c01e3746ca77
Qobi1 <tolaganov03@list.ru>
2022-05-23 15:22:00 +0500

    'm'

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
         'USER': 'maxshop',
         'PASSWORD': 'shop1234',
